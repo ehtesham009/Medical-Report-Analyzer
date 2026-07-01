c567741e12d487952aa58d182787a9ec5aad70c9
ehtesham009 <101001426+ehtesham009@users.noreply.github.com>
2026-07-02 02:15:52 +0500

    files upload

---
+++ project/src/views/Login.vue
@@ -0,0 +1,157 @@
+<template>
+  <div class="auth-container">
+
+    <div class="auth-card">
+
+      <div class="left">
+        <i class="fa-solid fa-user-doctor"></i>
+        <h2>Medical Report Analyzer</h2>
+        <p>
+          Welcome Back! Login to manage your reports securely.
+        </p>
+      </div>
+
+      <div class="right">
+
+        <h2>Login</h2>
+
+        <form @submit.prevent="login">
+
+          <input
+            type="email"
+            placeholder="Email"
+            v-model="email"
+          >
+
+          <input
+            type="password"
+            placeholder="Password"
+            v-model="password"
+          >
+
+          <button>
+            Login
+          </button>
+          <p class="bottom-text">
+  Don't have an account?
+  <router-link to="/register">
+    Register
+  </router-link>
+</p>
+
+        </form>
+
+      </div>
+
+    </div>
+
+  </div>
+</template>
+
+<script setup>
+
+import {ref} from "vue"
+import {useRouter} from "vue-router"
+
+const router=useRouter()
+
+const email=ref("")
+const password=ref("")
+
+function login(){
+
+const savedUser = JSON.parse(localStorage.getItem("user"))
+
+if(!savedUser){
+alert("Please register first!")
+return
+}
+
+if(
+savedUser.email===email.value &&
+savedUser.password===password.value
+){
+
+router.push("/dashboard")
+
+}else{
+
+alert("Invalid Email or Password")
+
+}
+
+}
+
+</script>
+
+<style scoped>
+
+.auth-container{
+display:flex;
+justify-content:center;
+align-items:center;
+height:85vh;
+}
+
+.auth-card{
+display:grid;
+grid-template-columns:1fr 1fr;
+width:900px;
+background:white;
+border-radius:20px;
+overflow:hidden;
+box-shadow:0 10px 30px rgba(0,0,0,.1);
+}
+
+.left{
+background:#2563EB;
+color:white;
+padding:60px;
+display:flex;
+flex-direction:column;
+justify-content:center;
+align-items:center;
+text-align:center;
+}
+
+.left i{
+font-size:70px;
+margin-bottom:20px;
+}
+
+.right{
+padding:50px;
+}
+
+.right h2{
+margin-bottom:20px;
+color:#1E3A8A;
+}
+
+button{
+width:100%;
+margin-top:15px;
+}
+
+@media(max-width:900px){
+
+.auth-card{
+grid-template-columns:1fr;
+}
+
+.left{
+display:none;
+}
+
+}
+.bottom-text{
+text-align:center;
+margin-top:15px;
+}
+
+.bottom-text a{
+color:#2563EB;
+font-weight:bold;
+text-decoration:none;
+}
+</style>
---
+++ project/src/views/Register.vue
@@ -0,0 +1,131 @@
+<template>
+  <div class="auth-container">
+
+    <div class="auth-card">
+
+      <h2>Create Account</h2>
+
+      <form @submit.prevent="register">
+
+        <input
+          type="text"
+          v-model="name"
+          placeholder="Full Name"
+          required
+        />
+
+        <input
+          type="email"
+          v-model="email"
+          placeholder="Email"
+          required
+        />
+
+        <input
+          type="password"
+          v-model="password"
+          placeholder="Password"
+          required
+        />
+
+        <button type="submit">
+          Register
+        </button>
+
+      </form>
+
+      <p class="bottom-text">
+        Already have an account?
+        <router-link to="/login">
+          Login
+        </router-link>
+      </p>
+
+    </div>
+
+  </div>
+</template>
+
+<script setup>
+import { ref } from "vue"
+import { useRouter } from "vue-router"
+
+const router = useRouter()
+
+const name = ref("")
+const email = ref("")
+const password = ref("")
+
+function register() {
+
+  const user = {
+    name: name.value,
+    email: email.value,
+    password: password.value
+  }
+
+  localStorage.setItem("user", JSON.stringify(user))
+
+  alert("Registration Successful!")
+
+  router.push("/login")
+}
+</script>
+
+<style scoped>
+
+.auth-container{
+display:flex;
+justify-content:center;
+align-items:center;
+height:90vh;
+}
+
+.auth-card{
+width:400px;
+background:white;
+padding:35px;
+border-radius:15px;
+box-shadow:0 10px 25px rgba(0,0,0,.1);
+}
+
+h2{
+text-align:center;
+margin-bottom:20px;
+color:#2563EB;
+}
+
+input{
+width:100%;
+padding:12px;
+margin-bottom:15px;
+border:1px solid #ddd;
+border-radius:8px;
+}
+
+button{
+width:100%;
+padding:12px;
+border:none;
+background:#2563EB;
+color:white;
+border-radius:8px;
+cursor:pointer;
+}
+
+button:hover{
+background:#1D4ED8;
+}
+
+.bottom-text{
+margin-top:15px;
+text-align:center;
+}
+
+.bottom-text a{
+color:#2563EB;
+text-decoration:none;
+font-weight:bold;
+}
+
+</style>
